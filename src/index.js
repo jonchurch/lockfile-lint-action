e10389e4f1822ba608c0dc340bb646075dd3d9d0
Jon Church <me@jonchurch.com>
2019-12-24 16:56:05 -0500

    log on failure

--- src/index.js
+++ src/index.js
@@ -50,7 +50,8 @@ try {
 
   if (failures.length) {
     const failMessage = failures.flat().join('\n');
-    setFailed(failMessage);
+		setFailed(failMessage);
+		console.log("===FAILURE!!!!!")
   } else {
     console.log('Passed validators');
   }
