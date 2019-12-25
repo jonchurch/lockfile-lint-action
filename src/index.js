fab498289be36d1977b2823c0495e8a5edff7e60
Jon Church <me@jonchurch.com>
2019-12-25 00:33:30 -0500

    cleanup src/index.js, create run function for main logic

--- src/index.js
+++ src/index.js
@@ -7,21 +7,10 @@ const {
 } = require('lockfile-lint-api');
 const { getInput, setFailed } = require('@actions/core');
 
-// //
-//
-//
-//
-//
-console.log('GH Worksspace: ', process.env.GITHUB_WORKSPACE);
-console.log('DIRNAME:', __dirname);
-console.log(
-  'MAYBE THE RIGHT DIR FOR PACKAGE-LOCK:',
-  path.join(__dirname, '../package-lock.json')
-);
-
 const lockPath =
   getInput('lockfilePath') ||
   path.join(process.env.GITHUB_WORKSPACE, 'package-lock.json');
+
 const defaultSchemes = ['https:', 'http:'];
 let schemes = getInput('schemes') || [];
 if (schemes.length) {
@@ -54,20 +43,25 @@ function gatherFailures(results) {
   });
   return failures;
 }
-try {
-  const schemeResults = schemeValidator.validate(schemes);
-  const hostResults = hostValidator.validate(['npm']);
-  const httpsResults = httpsValidator.validate();
 
-  const failures = gatherFailures([hostResults, httpsResults, schemeResults]);
+function run() {
+  try {
+    const schemeResults = schemeValidator.validate(schemes);
+    const hostResults = hostValidator.validate(['npm']);
+    const httpsResults = httpsValidator.validate();
 
-  if (failures.length) {
-    const failMessage = failures.flat().join('\n');
-    setFailed(failMessage);
-  } else {
-    console.log('Passed validators');
+    const failures = gatherFailures([hostResults, httpsResults, schemeResults]);
+
+    if (failures.length) {
+      const failMessage = failures.flat().join('\n');
+      setFailed(failMessage);
+    } else {
+      console.log('Passed validators');
+    }
+  } catch (error) {
+    console.log(error);
+    setFailed('Something went wrong during validation');
   }
-} catch (error) {
-  console.log(error);
-  setFailed('Something went wrong during validation');
 }
+
+run();
